26658f1fced4aee448349073e52096e6d8768bd7
YI ZHANG <scaccterran@gmail.com>
2019-12-27 14:54:10 -0500

    OPU-18 display file content on reader

---
+++ src/components/file-viewer/file-content-reader.vue
@@ -0,0 +1,14 @@
+<template>
+<pre>{{ content }}</pre>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+    props: ['content']
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/components/file-viewer/file-viewer.vue
+++ src/components/file-viewer/file-viewer.vue
@@ -1,11 +1,41 @@
 <template>
-    <div>file-viewer!</div>
+<file-content-reader :content="files.get(active)" />
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 
-export default Vue.extend({});
+import FileContentReader from './file-content-reader.vue';
+
+import IFileContent from '../../services/interfaces/file-content.interface';
+
+export default Vue.extend({
+    props: ['opened', 'preview'],
+    data: () => ({
+        active: ''
+    }),
+    components: {
+        FileContentReader
+    },
+    beforeMount(): void {
+        if (this.files.size) {
+            this.active = this.files.keys().next().value;
+        }
+    },
+    computed: {
+        files(): Map<string, Buffer> {
+            const map = new Map<string, Buffer>();
+
+            [...this.opened, this.preview].forEach((_: IFileContent) => {
+                if (_) {
+                    map.set(_.path, _.content);
+                }
+            });
+
+            return map;
+        }
+    }
+});
 </script>
 
 <style lang="scss" scoped>
