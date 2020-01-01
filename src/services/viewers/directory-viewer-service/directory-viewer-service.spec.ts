5ce10eb3ef1a3254cf5b9a42ac506d572f2956b3
YI ZHANG <scaccterran@gmail.com>
2019-12-31 21:29:00 -0500

    OPU-54 tests for directory viewer service

--- src/services/viewers/directory-viewer-service/directory-viewer-service.spec.ts
+++ src/services/viewers/directory-viewer-service/directory-viewer-service.spec.ts
@@ -21,14 +21,40 @@ context('directory viewer service unit test', () => {
     });
 
     describe('listDirectoryRecursive', () => {
+        it('should return all files under specified directory', async () => {
+            const [file1, file2] = ['file_name_1.ts', 'file_name_2.ts'];
+            directoryServiceStub.listDirectory.resolves([file1, file2]);
+            directoryServiceStub.isDirectory.resolves(false);
+
+            const result = await directoryViewerService.listDirectoryRecursive('');
+
+            expect(result.length).to.equal(2);
+            expect(result[0].name).to.equal(file1);
+            expect(result[1].name).to.equal(file2);
+        });
+
+        it('should return children for directories', async () => {
+            const [file1, file2, file3] = ['file_name_1.ts', 'file_name_2.ts', 'file_name_3.ts'];
+            directoryServiceStub.listDirectory.onFirstCall().resolves([file1]);
+            directoryServiceStub.listDirectory.onSecondCall().resolves([file2, file3]);
+            directoryServiceStub.isDirectory.onFirstCall().resolves(true);
+            directoryServiceStub.isDirectory.resolves(false);
+
+            const result = await directoryViewerService.listDirectoryRecursive('');
+
+            expect(result[0].name).to.equal(file1);
+            expect((result[0].children || []).length).to.equal(2);
+            expect((result[0].children || [])[0].name).to.equal(file2);
+            expect((result[0].children || [])[1].name).to.equal(file3);
+        });
+
         it('should not return any child for files', async () => {
             const file = 'file_name.ts'
             directoryServiceStub.listDirectory.resolves([file]);
             directoryServiceStub.isDirectory.resolves(false);
 
-            const result = await directoryViewerService.listDirectoryRecursive('path');
+            const result = await directoryViewerService.listDirectoryRecursive('');
 
-            expect(result.length).to.equal(1);
             expect(result[0].name).to.equal(file);
             expect(result[0].children).to.be.null;
         });
